c041259dfbf396e587a1c8b62408834ca0b3213d
han1202012 <octopus_truth@163.com>
2020-12-12 10:33:13 +0800

    update

---
+++ 001_ValueAnimator/src/main/java/kim/hsl/animator/MainActivity.kt
@@ -0,0 +1,25 @@
+package kim.hsl.animator
+
+import android.animation.ValueAnimator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 设置 0 到 360 之间的动画值
+        ValueAnimator.ofFloat(0F, 360F).apply {
+            // 设置动画持续时间
+            duration = 2000
+
+            addUpdateListener {
+                //textView.ro
+            }
+
+        }
+
+
+    }
+}
